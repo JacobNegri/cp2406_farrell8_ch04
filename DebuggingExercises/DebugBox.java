979f9449fa06a141be24d3dfc2e20835d1375e76
jacobnegri <jacob.negri@my.jcu.edu.au>
2016-08-22 08:38:39 +1000

    couple of changes to debugging excercises

--- DebuggingExercises/DebugBox.java
+++ DebuggingExercises/DebugBox.java
@@ -3,7 +3,7 @@ public class DebugBox
    private int width;
    private int length;
    private int height;
-   private FixDebugBox()
+   private DebugBox()
    {
       length = 1;
       width = 1;
