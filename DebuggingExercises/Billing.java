904b85484a48af27a4a4b0482f59c95e21247cf8
jacobnegri <jacob.negri@my.jcu.edu.au>
2016-08-22 08:36:59 +1000

    completed Billing - all tasks completed

--- DebuggingExercises/Billing.java
+++ DebuggingExercises/Billing.java
@@ -1,29 +1,38 @@
 // This class discounts prices by 10%
-public class FormLetterWriter
+public class Billing
 {
+   final static double TAX = 0.08;
    public static void main(String[] args)
    {
-      String name1, name2, lname2;
-      name1 = Negri;
-      name2 = Alex;
-      lname2 = Goulding
+      double bill;
 
-      displaySalutation(name1);
-      displayLetter();
-      displaySalutation(name2, lname2);
-      displayLetter();
-   }
-   public static void displaySalutation(String lastName)
-   {
-      System.out.println("Dear Mr. or Ms. " + lastName + ",");
+      bill = computeBill(42.00)
+      System.out.println("The total for a photobook that costs $42.00 is $" + bill);
+
+      bill = computeBill(19.99, 3);
+      System.out.println("The total for three photobooks that cost $19.99 is $" + bill);
+
+      bill = computeBill(12.50, 8, 20.00);
+      System.out.println("The total for eight photobooks that cost $12.50 with a $20 coupon is $" + bill);
    }
 
-   public static void displaySalutation(String firstName, String lastName)
+   public static double computeBill(double price)
    {
-      System.out.println("Dear " + firstName + " " + lastName + ",");
+      double bill = price + (price * TAX);
+      return bill;
    }
-   public static void displayLetter()
+
+   public static double computeBill(double price, int quantity)
    {
-      System.out.println("   Thank you for your recent order.\n");
+      double bill = price * quantity;
+      bill = bill + (bill * TAX);
+      return bill;
+   }
+   public static double computeBill(double price, int quantity, double coupon)
+   {
+      double bill = price * quantity;
+      bill = bill - coupon;
+      bill = bill + (bill * TAX);
+      return bill;
    }
 }
